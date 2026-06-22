258bcad15c7b2d4fe40fdf9fd7ffdd14e429e506
Andrewww24 <andrewriveragam24@gmail.com>
2026-06-22 16:51:57 -0600

    conflictos
    Resueltos

--- includes/header.php
+++ includes/header.php
@@ -30,11 +30,13 @@ $baseUrl = $baseUrl ?? '.';
                 <li class="nav-item">
                     <a class="nav-link" href="<?= $baseUrl ?>/grupos/index.php">Grupos</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= $baseUrl ?>/tareas/tablero.php">Tablero</a>
+                </li>
             </ul>
         </div>
     </div>
 </nav>
-
 <main class="container">
 <?php if (!empty($_GET['msg'])): ?>
     <div class="alert alert-success alert-dismissible fade show">
@@ -42,7 +44,6 @@ $baseUrl = $baseUrl ?? '.';
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
     </div>
 <?php endif; ?>
-
 <?php if (!empty($_GET['error'])): ?>
     <div class="alert alert-danger alert-dismissible fade show">
         <?= htmlspecialchars($_GET['error']) ?>
